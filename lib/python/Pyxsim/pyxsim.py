82051038769feb1f537f32b5adc9cd61dd84c5e5
Kevin Yeung <kevinyeung@xmos.com>
2022-01-04 18:08:58 +0800

    conflict resolved

--- lib/python/Pyxsim/pyxsim.py
+++ lib/python/Pyxsim/pyxsim.py
@@ -251,7 +251,7 @@ class Xsi:
         self._simthreads = []
         self._time = 0
         self.xe = Xe(self.xe_path)
-        self._time_step = 1000.0 / self.xe.freq
+        self._time_step = 1000000000.0 / self.xe.freq #time-step in fs
 
     def register_plugin(self, plugin):
         self._plugins.append(plugin)
