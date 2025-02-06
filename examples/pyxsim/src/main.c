369b06e0b1c991647e6466cab2cf01ff30e350f7
Daniel Pieczko <danielpieczko@xmos.com>
2025-02-06 14:34:32 +0000

    Update copyright years

--- examples/pyxsim/src/main.c
+++ examples/pyxsim/src/main.c
@@ -1,3 +1,5 @@
+// Copyright 2025 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #include <print.h>
 #include <xcore/port.h>
 #include <xcore/select.h>
